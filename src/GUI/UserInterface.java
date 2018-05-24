e0e7011487a541ff59830905002acbdbfc91bb7c
Christopher Ayling <chris.ayling@blackbook.ai>
2018-05-24 05:40:10 +0100

    added a whole bunch of broken code to the GUI, added a few more comments in GUI

--- src/GUI/UserInterface.java
+++ src/GUI/UserInterface.java
@@ -6,6 +6,12 @@ package GUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.EventListener;
 
 import Store.*;
@@ -23,10 +29,15 @@ import javax.swing.*;
  */
 public class UserInterface {
 
-	public static Store store = new Store();
+	public static Store store = Store.getInstance();
 	
+	/**
+	 * Constructor
+	 */
 	public UserInterface() {
 	}
+	
+	
 	
 	/**
 	 * Create the show the graphical user interface.
@@ -47,7 +58,7 @@ public class UserInterface {
         // add capital label
         JLabel capitalLabel = new JLabel("Capital: " + store.displayCapital());
         topPanel.add(capitalLabel);
-        
+
         //add buttons
         JButton loadItemPropertiesButton = new JButton("Load Item Properties");
         loadItemPropertiesButton.addActionListener(new ActionListener() {
@@ -73,6 +84,10 @@ public class UserInterface {
         	 */
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				//find all items where amount in stock is less than reorder point
+				//create manifest to order the the reorder quantity of each item which minimizes cost
+				//save as a CSV to a location of the user's choice
+				
 				// TODO Create an optimised manifest object from current inventory.
 					// TODO get
 				
@@ -81,8 +96,12 @@ public class UserInterface {
 				fd.setVisible(true);
 				String path = fd.getFile();
 				// TODO Write manifest to CSV and save to location.
-				Manifest manifest = new Manifest(store.getToRestock());
-				manifest.writeToCSV(path);
+				Manifest manifest = new Manifest();
+				try {
+					manifest.writeToCSV(path);
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(frame, "Error writing manifest to CSV");
+				}
 				
 				System.out.println("Export manifest button clicked");
 			}
@@ -97,28 +116,124 @@ public class UserInterface {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Load manifest button clicked");
-				// TODO User selects file to load manifest from.
+				// present user with file selection dialog
+				FileDialog fd = new FileDialog(frame, "Load Manifest", FileDialog.SAVE);
+				fd.setVisible(true);
+				String path = fd.getFile();
 				
-				// Read CSV and create manifest object.
-				Manifest manifest = new Manifest("TODO get path");
-				// Decrease capital based on item costs in manifest.
-				store.addCapital(-manifest.getCost());
-					// TODO update capital on GUI
-				// TODO Increase inventory based on item in manifest.
-				store.restock(manifest.getItems());
-			}
+				Truck truck;
+				Item item;
+				Stock stock;
+				
+				FileReader reader = new FileReader(path);
+				BufferedReader bufferedReader = new BufferedReader(reader);
+				String line;
+				
+				Manifest manifest = new Manifest();
+				
+				java.util.List<String> rows;
+				try {
+					rows = Files.readAllLines(Paths.get(path));
+					for (int i=0; i<rows.size(); i++) {
+						switch (rows.get(i)) {
+						case ">Ordinary":
+							truck = new OrdinaryTruck();
+							for (; i<rows.size(); i++) {
+								
+							}
+							break;
+						case ">Refrigerated":
+							truck = new RefrigeratedTruck();
+							break;
+						default:
+							break;
+						}
+					}
+				} catch (IOException e3) {
+					e3.printStackTrace();
+				}
+				
+				
+				
+				try {
+					
+					for (String row : Files.readAllLines(Paths.get(path))) {
+						stock = new Stock();
+						// create a new truck
+						switch (row) {
+						case ">Ordinary":
+							truck = new OrdinaryTruck();
+							break;
+						case ">Refrigerated":
+							truck = new RefrigeratedTruck();
+							break;
+						default:
+							break;
+						}
+						// get the items
+						String[] itemData = row.split(",");
+						item = new Item("test", 10, 1, 10, 10);
+						store.getInventory().add(item);
+						stock.add(item);
+					}// end for
+					
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(frame, "IOException");
+				} catch (CSVFormatException e2){
+					JOptionPane.showMessageDialog(frame, "CSV is in the wrong format");
+				}
+				
+				double cost = 0;
+				while ((line = bufferedReader.readLine()) != null) {
+					stock = new Stock();
+					switch (line) { // create a truck
+					case ">Ordinary":
+						truck = new OrdinaryTruck();
+						break;
+					case ">Refrigerated":
+						truck = new RefrigeratedTruck();
+						break;
+					default:
+						JOptionPane.showMessageDialog(frame, "Your CSV is in the wrong format!");
+						break;					
+					}//end switch
+					
+					while ((line = bufferedReader.readLine()) != null) {// fill the truck
+						String[] itemData = line.split(","); // itemData[0] is name and itemData[1] is quantity
+//						item = store.getItem(itemData[0]);
+						item = new Item("test", 10.0, 0.1, 10, 20);
+						store.getInventory().add(item);
+						stock.add(item);
+					}//end while
+					truck.loadCargo(stock);
+					cost += truck.getCost();
+					store.addCapital(-cost);
+					capitalLabel.setText("Capital: " + store.displayCapital());
+				}//end while
+				bufferedReader.close();
+				
+				// update capital shown in GUI
+				capitalLabel.setText("Capital: " + store.displayCapital());
+				// update item quantities in GUI
+				
+				frame.repaint();			
+			}//end action performed
 		});
         
         JButton loadSalesLogButton = new JButton("Load Sales Log");
         loadSalesLogButton.addActionListener(new ActionListener() {
         	
         	/**
-        	 * Load in a sales log; increases capital and decreases inventory.
+        	 * Load in a sales log; increases capital and  inventory.
         	 */
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Load Sales
 				System.out.println("Load Sales Log button clicked");
+				
+				store.addCapital(100);
+				capitalLabel.setText("10000000000");
+				frame.repaint();
 			}
 		});
         
@@ -126,6 +241,9 @@ public class UserInterface {
         topPanel.add(exportManifestButton);
         topPanel.add(loadManifestButton);
         topPanel.add(loadSalesLogButton);
+        exportManifestButton.setEnabled(false);
+        loadManifestButton.setEnabled(false);
+//        loadSalesLogButton.setEnabled(false);
         mainPanel.add(topPanel, BorderLayout.NORTH);
         
         // create and add table to centre
